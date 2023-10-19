a334a11bea84cb7c5168bb7ef9a79ad062b21414
gustigiyus <gustiggt123@gmail.com>
2023-10-19 16:37:07 +0700

    Database Migration

---
+++ database/migrations/2023_10_19_084925_create_penduduks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('penduduks', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama', 100);
+            $table->string('nik', 25);
+            $table->date('tgl_lahir');
+            $table->enum('jns_kelamin();', ['Laki-laki', 'Perempuan']);
+            $table->text('alamat');
+            $table->foreignId('provinsi_id')->constrained()->onUpdate('cascade')->onDelete('restrict');
+            $table->foreignId('kabupaten_id')->constrained()->onUpdate('cascade')->onDelete('restrict');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('penduduks');
+    }
+};
